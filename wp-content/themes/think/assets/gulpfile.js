8ec88d8cd8a20d188277cae3db4d2bfa176aa253
unknown <josh@theswig.ca>
2015-10-25 22:20:48 -0300

    Red

--- wp-content/themes/think/assets/gulpfile.js
+++ wp-content/themes/think/assets/gulpfile.js
@@ -1,15 +1,16 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var nano = require('gulp-cssnano');
+var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    rucksack = require('gulp-rucksack'),
+    nano = require('gulp-cssnano');
 
 // PostCSS Plugins
-var includes = require('postcss-import');
-var mixins = require('postcss-mixins');
-var forLoops = require('postcss-for');
-var nested = require('postcss-nested');
-var autoprefixer = require('autoprefixer-core');
-var simpleVars = require('postcss-simple-vars');
-var colorFunction = require('postcss-color-function');
+var includes = require('postcss-import'),
+    mixins = require('postcss-mixins'),
+    forLoops = require('postcss-for'),
+    nested = require('postcss-nested'),
+    autoprefixer = require('autoprefixer-core'),
+    simpleVars = require('postcss-simple-vars'),
+    colorFunction = require('postcss-color-function');
 
 gulp.task('compile', function() {
     var processors = [
@@ -23,18 +24,19 @@ gulp.task('compile', function() {
     ];
     return gulp.src('./post/*.css')
         .pipe(postcss(processors))
+        .pipe(rucksack())
         .pipe(gulp.dest('./css'));
 });
 
 gulp.task('nano', ['compile'], function () {
-    return gulp.src('./css/*.css')
+    return gulp.src(['./css/*.css', '!./css/styleguide.css'])
         .pipe(nano())
         .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./post/*.css', ['compile', 'nano']);
-    gulp.watch('./post/*/*.css', ['compile', 'nano']);
+    gulp.watch('post/*.css', ['compile', 'nano']);
+    gulp.watch('post/*/*.css', ['compile', 'nano']);
 });
 
 gulp.task('default', ['watch']);
--- wp-content/themes/think/assets/js/app.js
+++ wp-content/themes/think/assets/js/app.js
@@ -2,7 +2,7 @@
 
     // APP JS
     // Property of Norex
-    // Authored by Josh Beveridge
+    // Authored by Josh Beveridge and Justin Bellefontaine
 
 // =============================================================================
 
@@ -10,15 +10,21 @@
 
     $(document).ready(function() {
 
-        // Fixed Width =========================================================
-        var documentWidth = $(document).outerWidth();
-
-        $('html').css('width', documentWidth);
-        $('body').css('width', documentWidth);
+        // User Agent Data Attributes ==========================================
+        var ua = navigator.userAgent;
+        ua = ua.toString();
+        $('body').attr('id', ua);
 
         // BuggyFill Initialization ============================================
         window.viewportUnitsBuggyfill.init({
             hacks: window.viewportUnitsBuggyfill.hacks
+        });
+
+        // Disabled Button Clicks ==============================================
+        $('.disabled').on('click', function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
         });
 
         // Smooth Scrolling ====================================================
@@ -35,7 +41,7 @@
                  if (target.length) {
                    $('html,body').animate({
                      scrollTop: target.offset().top - $('.search-cta').outerHeight()
-                   }, 650);
+                   }, 650); // Duration
                    return false;
                  }
 
@@ -46,7 +52,7 @@
          });
 
         // Data Link Handler ===================================================
-        $('[data-link-handler]').on('click touchend',function(e){
+        $('[data-link-handler]').on('click',function(e){
             if($('[data-link-target="'+$(this).attr('data-link-handler')+'"]').hasClass('active')) {
                 $('[data-link-target="'+$(this).attr('data-link-handler')+'"]').removeClass('active');
             }
@@ -56,160 +62,22 @@
             e.preventDefault();
         });
 
-        // Navigation Handler ==================================================
-        $('.menu .close').on('click', function(e) {
-            if($(this).hasClass('active')) {
-                e.preventDefault();
-            }
-            else {
-                location.href = 'http://joshbeveridge.com';
-            }
-        });
-
-        $('.close-area').on('click', function(e) {
-            $('.navigation-content').removeClass('active');
-            $('.navigation').removeClass('active');
-            $('.menu button').removeClass('active');
-            if($('.why').hasClass('active')) {
-                $('.hero').removeClass('active');
-                $('article').removeClass('active');
-            }
-            else {
-                $('body').removeClass('stop');
-                $('.hero').removeClass('active');
-                $('article').removeClass('active');
-            }
+        // Navigation Handlers =================================================
+        $('#menuButton').on('click', function(e){
+            $('nav').addClass('active');
+            $('.content').removeClass('active');
+            $('#menuButton').addClass('active');
+            $('#contentButton').removeClass('active');
             e.preventDefault();
         });
 
-        $('.menu button').on('click', function(e) {
-            if($(this).hasClass('disable')){
-                e.preventDefault();
-            }
-            else {
-                if($(this).hasClass('close') || $(this).hasClass('active')) {
-                    $('.navigation-content').removeClass('active');
-                    $('.navigation').removeClass('active');
-                    $('.menu button').removeClass('active');
-                    $(this).removeClass('active');
-                    if($('.why').hasClass('active')) {
-                        $('.hero').removeClass('active');
-                        $('article').removeClass('active');
-                    }
-                    else {
-                        $('body').removeClass('stop');
-                        $('.hero').removeClass('active');
-                        $('article').removeClass('active');
-                    }
-                    e.preventDefault();
-                }
-                else {
-                    if($('.navigation').hasClass('active')) {
-                        $('.navigation-content').removeClass('active');
-                        var buttonID = $(this).attr('id');
-                        var buttonClass = '.' + buttonID;
-                        $('.menu button').removeClass('active');
-                        $('#navClose').addClass('active');
-                        $(this).addClass('active');
-                        $(buttonClass).addClass('active');
-                        e.preventDefault();
-                    }
-                    else {
-                        var buttonID = $(this).attr('id');
-                        var buttonClass = '.' + buttonID;
-                        $('.navigation').addClass('active');
-                        $('#navClose').addClass('active');
-                        $(this).addClass('active');
-                        $(buttonClass).addClass('active');
-                        $('body').addClass('stop');
-                        $('.hero').addClass('active');
-                        $('article').addClass('active');
-                        // $('.hero').removeClass('active');
-                        // $('article').removeClass('active');
-                        e.preventDefault();
-                    }
-                }
-                e.preventDefault();
-            }
+        $('#contentButton').on('click', function(e){
+            $('nav').removeClass('active');
+            $('.content').addClass('active');
+            $('#menuButton').removeClass('active');
+            $('#contentButton').addClass('active');
             e.preventDefault();
         });
-
-        // Hero Animation ======================================================
-        // $('#recent').on('click', function(e) {
-        //     $('.hero').addClass('active');
-        //     $('article').addClass('active');
-        // });
-        //
-        // if($('section').hasClass('hero')) {
-        //     $(window).scroll(function(e) {
-        //         if($(window).scrollTop() > 0) {
-        //             $('.hero').addClass('active');
-        //             $('article').addClass('active');
-        //         }
-        //         if($(window).scrollTop() == 0) {
-        //             $('.hero').removeClass('active');
-        //             $('article').removeClass('active');
-        //         }
-        //     });
-        // }
-        // else {
-        //     $('article').addClass('active');
-        // }
-
-        // Why Handler =========================================================
-        $('#why').on('click', function(e) {
-            $('.hero >.wrapper').addClass('hidden');
-            $('#recent').addClass('hidden');
-            $('body').addClass('stop');
-            $('.why').addClass('active');
-        });
-
-        $('#whyClose').on('click', function(e) {
-            $('.hero >.wrapper').removeClass('hidden');
-            $('#recent').removeClass('hidden');
-            $('body').removeClass('stop').removeClass('move');
-            $('.why').removeClass('active');
-        });
-
-        // Why Content Handler =================================================
-        $(".why-menu button:not('#whyClose')").on('click', function(e) {
-            $('.why-menu dd').removeClass('active');
-            $('.why-content p').removeClass('active');
-            var buttonID = $(this).attr('id');
-            var buttonClass = '.' + buttonID;
-            $(buttonClass).addClass('active');
-            $(this).parent().addClass('active');
-        });
-
-        // Mobile Sizes ========================================================
-        if (window.matchMedia("(orientation: portrait)").matches) {
-            var navigationHeight = $('.navigation').outerHeight();
-            var paneHeight = $('.pane').outerHeight();
-            var heroHeight = $('.hero').outerHeight();
-            var whyHeight = $('.why').outerHeight();
-            $('.navigation').css('height', navigationHeight + 'px');
-            $('.pane').css('height', paneHeight + 'px');
-            $('.hero').css('height', heroHeight + 'px');
-            $('.why').css('height', whyHeight + 'px');
-        }
-
-    });
-
-    $(window).load(function(){
-
-        if (window.matchMedia("(orientation: landscape)").matches) {
-
-            var maxWhyHeight = 0;
-
-            $('.why-content p').each(function() {
-                if($(this).outerHeight(true) > maxWhyHeight) {
-                    maxWhyHeight = $(this).outerHeight(true);
-                }
-                console.log(maxWhyHeight);
-                $('.why-content').css('height', maxWhyHeight + 'px');
-            });
-
-        }
 
     });
 
