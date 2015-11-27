69a28ff94973da44e687e5f55878dea4829a3098
Josh Beveridge <me@joshbeveridge.com>
2015-11-27 15:37:47 -0400

    Desktop Summary Scrollability and Listing Background Colours

--- wp-content/themes/levelup/single.php
+++ wp-content/themes/levelup/single.php
@@ -1,4 +1,14 @@
 <?php
-wp_redirect('http://joshbeveridge.com/');
+
+$postSlug = basename(get_the_permalink());
+
+if(in_category("design")) {
+    $postLinkTemp = "http://joshbeveridge.com/#designer-post-".$postSlug;
+}
+if(in_category("gaming")) {
+    $postLinkTemp = "http://joshbeveridge.com/#gamer-post-".$postSlug;
+}
+
+wp_redirect($postLinkTemp);
 exit;
 ?>
